677c049245fd9a7d728a2bff79e07adf060f2400
michalnand <michal.nand@gmail.com>
2023-03-28 17:52:35 +0200

    tutorial

--- LibsControlEmbedded/matmul.h
+++ LibsControlEmbedded/matmul.h
@@ -21,13 +21,15 @@ void mm(DType *y, DType *a, DType *b, DType *c, unsigned int M, unsigned int N, 
             {
                 sum+= a[m*K + k]*b[k*N + n];
             }
+
+            y[m*N + n] = alpha*sum;
         }
 
     if (c != nullptr)
     {
         for (unsigned int i = 0; i < M*N; i++)
         {
-            y[i]+= c[i];
+            y[i]+= beta*c[i];
         }
     }
 }
@@ -46,7 +48,7 @@ void mmt(DType *y, DType *a, DType *b, DType *c, DType alpha = 1, DType beta = 1
                 sum+= a[m*K + k]*b[k*N + n];
             }
 
-            y[m*N + n] = sum*alpha;
+            y[m*N + n] = alpha*sum;
         }
 
     
@@ -55,9 +57,112 @@ void mmt(DType *y, DType *a, DType *b, DType *c, DType alpha = 1, DType beta = 1
     {
         for (unsigned int i = 0; i < M*N; i++)
         {
-            y[i]+= c[i]*beta;
+            y[i]+= beta*c[i];
         }
     }
 }
+
+
+template<class DType>
+void fill(DType *x, unsigned int M, unsigned int N, DType value)
+{
+    for (unsigned int i = 0; i < M*N; i++)
+    {
+        x[i] = value;
+    }
+}
+
+template<unsigned int M, unsigned int N, class DType>
+void fill(DType *x, DType value)
+{
+    for (unsigned int i = 0; i < M*N; i++)
+    {
+        x[i] = value;
+    }
+}
+
+
+
+template<class DType>
+void copy(DType *dest, DType *src, unsigned int M, unsigned int N)
+{
+    for (unsigned int i = 0; i < M*N; i++)
+    {
+        dest[i] = src[i];
+    }
+}
+
+template<unsigned int M, unsigned int N, class DType>
+void copy(DType *dest, DType *src)
+{
+    for (unsigned int i = 0; i < M*N; i++)
+    {
+        dest[i] = src[i];
+    }
+}
+
+
+
+
+template<class DType>
+void add(DType *y, DType *a, DType *b, unsigned int M, unsigned int N)
+{
+    for (unsigned int i = 0; i < M*N; i++)
+    {
+        y[i] = a[i] + b[i];
+    }
+}
+
+template<unsigned int M, unsigned int N, class DType>
+void add(DType *y, DType *a, DType *b)
+{
+    for (unsigned int i = 0; i < M*N; i++)
+    {
+        y[i] = a[i] + b[i];
+    }
+}
+
+
+template<class DType>
+void sub(DType *y, DType *a, DType *b, unsigned int M, unsigned int N)
+{
+    for (unsigned int i = 0; i < M*N; i++)
+    {
+        y[i] = a[i] - b[i];
+    }
+}
+
+template<unsigned int M, unsigned int N, class DType>
+void sub(DType *y, DType *a, DType *b)
+{
+    for (unsigned int i = 0; i < M*N; i++)
+    {
+        y[i] = a[i] - b[i];
+    }
+}
+
+
+
+
+
+template<class DType>
+void mac(DType *y, DType *a, DType *b, DType alpha, DType beta, unsigned int M, unsigned int N)
+{
+    for (unsigned int i = 0; i < M*N; i++)
+    {
+        y[i] = y[i] + alpha*a[i] + beta*b[i];
+    }
+}
+
+template<unsigned int M, unsigned int N, class DType>
+void mac(DType *y, DType *a, DType *b, DType alpha, DType beta)
+{
+    for (unsigned int i = 0; i < M*N; i++)
+    {
+        y[i] = y[i] + alpha*a[i] + beta*b[i];
+    }
+}
+
+
 
 #endif
